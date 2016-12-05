fd577b24c48b51b88cbe6ce12b3d6df27ff4f60c
Dog2puppy <colecooper2005@icloud.com>
2016-12-04 20:07:15 -0500

    Update Main.php

--- src/Dog2puppy/MyMoney/Main.php
+++ src/Dog2puppy/MyMoney/Main.php
@@ -2,9 +2,18 @@
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
+use pocketmine\event\block\SignEventChange;
+use pocketmine\utils\Config;
 
 class Main extends PluginBase implements Listener{
-
+   public function onLoad(){
+     $this->getLogger()->info("Loaded MyMoney!");
+   }
+   public function onEnable(){
+     $this->config=new Config
+     
+     $this->getLogger()->info("Enabled MyMoney!");
+   }
 }
 
 ?>
