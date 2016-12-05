00c258262c06b5004625e0c541382ee39459f609
Dog2puppy <colecooper2005@icloud.com>
2016-12-05 07:07:49 -0500

    Update Main.php

--- src/Dog2puppy/MyMoney/Main.php
+++ src/Dog2puppy/MyMoney/Main.php
@@ -7,12 +7,19 @@ use pocketmine\utils\Config;
 
 class Main extends PluginBase implements Listener{
    public function onLoad(){
-     $this->getLogger()->info("Loaded MyMoney!");
+      $this->getLogger()->info("Loaded MyMoney.");
    }
    public function onEnable(){
-     $this->config=new Config
-     
-     $this->getLogger()->info("Enabled MyMoney!");
+      $this->saveDefaultConfig();
+      $this->config=new Config($this->getDataFolder."config.yml", Config::YAML);
+      $this->config->reload();
+      @mkdir($this->getDataFolder());
+      $this->config = new Config ($this->getDataFolder() . "config.yml" , Config::YAML);
+      $this->getServer()->getPluginManager()->registerEvents($this, $this);
+      $this->getLogger()->info("Enabled MyMoney.");
+   }
+   public function onDisable(){
+      $this->getLogger()->info("Disabled MyMoney.");
    }
 }
 
