44dd7cc793cd2c95f1b41c7c74633c4ee3355c21
Dog2puppy <colecooper2005@icloud.com>
2016-12-05 18:03:17 -0500

    Update Main.php

--- src/Dog2puppy/MyMoney/Main.php
+++ src/Dog2puppy/MyMoney/Main.php
@@ -5,16 +5,18 @@ use pocketmine\event\Listener;
 use pocketmine\event\block\SignEventChange;
 use pocketmine\utils\Config;
 
+namespace Dog2puppy\MyMoney;
+
 class Dog2puppy\MyMoney\Main extends PluginBase implements Listener{
    public function onLoad(){
       $this->getLogger()->info("Loaded MyMoney.");
    }
    public function onEnable(){
+      @mkdir($this->getDataFolder());
       $this->saveDefaultConfig();
       $this->config=new Config($this->getDataFolder."config.yml", Config::YAML);
       $this->config->reload();
-      @mkdir($this->getDataFolder());
-      $this->config = new Config ($this->getDataFolder() . "config.yml" , Config::YAML);
+      $this->players = new Config ($this->getDataFolder() . "players.yml" , Config::YAML);
       $this->getServer()->getPluginManager()->registerEvents($this, $this);
       $this->getLogger()->info("Enabled MyMoney.");
    }
