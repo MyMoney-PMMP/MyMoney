ef5ef42539abf51f177a129a323bd9140e21444d
Dog2puppy <colecooper2005@icloud.com>
2016-12-22 18:13:34 -0500

    Create API.php

---
+++ src/Dog2puppy/MyMoney/API.php
@@ -0,0 +1,14 @@
+<?php
+
+Namespace Dog2puppy\MyMoney;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+
+class API extends PluginBase{
+
+   public function addMoney($player, $ammount) {
+      
+   }
+
+}
