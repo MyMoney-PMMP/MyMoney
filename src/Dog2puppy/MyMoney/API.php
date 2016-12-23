02e1cc81be22f59134b62e32aa880af2bee7ee2f
Dog2puppy <colecooper2005@icloud.com>
2016-12-23 17:40:58 -0500

    Update API.php

--- src/Dog2puppy/MyMoney/API.php
+++ src/Dog2puppy/MyMoney/API.php
@@ -6,7 +6,12 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
 
 class API extends PluginBase{
-
+   
+   public function onEnable(){
+      @mkdir($this->getDataFolder());
+      $this->users= new Conig($this->getDataFolder()."users.yml", Config::YAML, array());
+   }
+   
    public function addMoney($player, $ammount) {
       
    }
