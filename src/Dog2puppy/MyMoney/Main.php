81c645e99df1d3a0c0a824912c3ce38bcd2cfe36
Dog2puppy <colecooper2005@icloud.com>
2016-12-23 18:05:38 -0500

    Update Main.php

--- src/Dog2puppy/MyMoney/Main.php
+++ src/Dog2puppy/MyMoney/Main.php
@@ -4,6 +4,10 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\event\block\SignEventChange;
 use pocketmine\utils\Config;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+use Dog2puppy\MyMoney\API;
 
 namespace Dog2puppy\MyMoney;
 
@@ -19,6 +23,7 @@ class Main extends PluginBase implements Listener {
       $this->config= new Config($this->getDataFolder."config.yml", Config::YAML);
       $this->config->reload();
       $this->players = new Config ($this->getDataFolder() . "players.yml" , Config::YAML);
+      $API= new API();
       $this->getServer()->getPluginManager()->registerEvents($this, $this);
       $this->getLogger()->info("Enabled MyMoney.");
    }
@@ -27,4 +32,14 @@ class Main extends PluginBase implements Listener {
       $this->getLogger()->info("Disabled MyMoney.");
    }
    
+   public function onCommand(CommandSender $sender,Command $cmd,$label,array $args){
+      switch($cmd){
+         case "givemoney":
+            $API->addMoney($args[1], $args[2]);
+            return true;
+         default:
+            return false;
+            break;
+      }
+   }
 }
