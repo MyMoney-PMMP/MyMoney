e42bf6e8c5b9f1ea1865941e7dd3174cee46407f
Dog2puppy <colecooper2005@icloud.com>
2016-12-31 13:21:13 -0800

    Update API.php

--- src/Dog2puppy/MyMoney/API.php
+++ src/Dog2puppy/MyMoney/API.php
@@ -10,8 +10,7 @@ class API extends PluginBase{
    public function onEnable(){
       @mkdir($this->getDataFolder());
       $this->config= new Config($this->getDataFolder()."config.yml", Config::YAML, array());
-      $unique-files=$this->config->get("general")["unique-files"];
-      if ($unique-files == true) {
+      if ($this->config->get("general")["unique-files"] == true) {
          @mkdir($this->getDataFolder()."players");
       }else{
          $this->users= new Config($this->getDataFolder()."users.yml", Config::YAML, array());
@@ -19,30 +18,30 @@ class API extends PluginBase{
    }
    
    public function getPlayer($player) {
-      if ($unique-files == true) {
+      if ($this->config->get("general")["unique-files"] == true) {
          return new Config($this->getDataFolder()."players/".$player.".yml", Config::YAML, array());
       }else{
          return $this->users;
       }
    
    public function addMoney($player, $ammount) {
-      if(!$this->users->get($player) === null){
-         $this->users->set($player, $this->users->get($player)+$ammount);
+      if(!$this->getPlayer($player)->get($player) === null){
+         $this->getPlayer($player)->set($player, $this->getPlayer($player)->get($player)+$ammount);
       }else{
-         $this->users->set($player, $ammount);
+         $this->getPlayer($player)->set($player, $ammount);
       }
       return true;
    }
    
    public function setMoney($player, $ammount) {
-      $this->users->set($player, $ammount);
+      $this->getPlayer($player)->set($player, $ammount);
       return true;
    }
    
    public function takeMoney($player, $ammount) {
-      if (!$this->users->get($player)) {
-         if (!$this->users->get($player) < $ammount) {
-            $this->users->set($player, $this->users->get($player) - $ammount);
+      if (!$this->getPlayer($player)->get($player)) {
+         if (!$this->getPlayer($player)->get($player) < $ammount) {
+            $this->getPlayer($player)->set($player, $this->getMoney($player)->get($player) - $ammount);
             return true;
          }
       }
